74a570219d8dee426e82edea26a705eb68240f75
TristanLoreau <tristan.loreau@laposte.net>
2021-09-28 15:39:40 +0200

    almost done

---
+++ guess-the-number-console-chrome/main.js
@@ -0,0 +1,18 @@
+console.log('Hello user !')
+console.log('Try to guess the mystery number.')
+console.log('Use "submit(x) to submit a number.')
+
+const mysteryNumber = 45
+
+const submit = (x) => {
+    if (x < mysteryNumber) {
+        console.log('Too small :).')
+    }
+    if (x > mysteryNumber) {
+            console.log('Too big :)')
+    }
+
+    if (x == mysteryNumber) {
+                console.log('Congratulations !')
+    }
+}
