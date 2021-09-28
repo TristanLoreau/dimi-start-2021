ff20c082291505e0e692efc3f12036f4abd16c25
TristanLoreau <tristan.loreau@laposte.net>
2021-09-28 16:57:32 +0200

    guess the number chrome end

--- guess-the-number-console-chrome/main.js
+++ guess-the-number-console-chrome/main.js
@@ -2,17 +2,29 @@ console.log('Hello user !')
 console.log('Try to guess the mystery number.')
 console.log('Use "submit(x) to submit a number.')
 
-const mysteryNumber = 45
+const mysteryNumber = Math.round(Math.random() * 100)
 
 const submit = (x) => {
     if (x < mysteryNumber) {
         console.log('Too small :).')
+        document.body.style.backgroundColor ='#25ff40'
+        document.body.innerHTML ='<h1>Too small</h1>'
+
     }
     if (x > mysteryNumber) {
             console.log('Too big :)')
+            document.body.style.backgroundColor ='#ff1818'
+            document.body.innerHTML ='<h1>Too big</h1>'
+
     }
 
     if (x == mysteryNumber) {
                 console.log('Congratulations !')
+                document.body.style.backgroundColor ='#ffe007'
+                document.body.innerHTML ='<h1>Bravo</h1>'
     }
+}
+
+const cheat =() => {
+    console.log(`The mystery number is ${mysteryNumber}`)
 }
