11c67eb934d99f4fc651e3872e2b0555971a150e
TristanLoreau <tristan.loreau@laposte.net>
2022-01-04 11:42:29 +0100

    test

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # dimi-start-2021
+
+[projet cours (joseph.m)](https://github.com/jniac/dimi-start-2021)
+
 ## pré-requis
 1. [git](https://git-scm.com/download)
 1. [node.js](https://nodejs.org/fr/)
 1. compte github
 1. Visual Studio Code
 1. Google Chrome Dev
+
+## autres
+
+### MarkDown 
+Aperçu du MarkDown (.md) dans VSCode :
+```
+Ctrl/command + Shift + V
+```
+
+### Plugins & thème dans VSCode
+Accès aux settings :
+```
+Ctrl/command + ,
+```
--- guess-the-number-web/main.js
+++ guess-the-number-web/main.js
@@ -1,2 +1,47 @@
+const response = document.querySelector('div.response')
+  response.remove()
+
 const mysteryNumber = Math.round(Math.random() * 100)
 
+
+const cloneResponse = (inputValue, commentValue) => {
+  const clone = response.cloneNode(true)
+  document.body.append(clone)
+  clone.querySelector('span.input').innerHTML = inputValue
+  clone.querySelector('span.comment').innerHTML = commentValue
+}
+
+const submit = (x) => { 
+  const input = document.querySelector('input')
+  const inputNumber = parseFloat(input.value)
+
+  if (isNaN(inputNumber)) {
+
+    cloneResponse(input.value, `Ceci n'est pas un nombre`)
+
+  }  else if (inputNumber < 0 || inputNumber > 100) {
+
+    cloneResponse(input.value, `Le nombre doit être compris entre 0 et 100.`)
+
+  } else if (inputNumber < mysteryNumber) {
+    cloneResponse(input.value, `Trop petit.`)
+
+  } else if (inputNumber > mysteryNumber) {
+    cloneResponse(input.value, `Trop grand.`)
+
+  } else if(inputNumber === mysteryNumber) {
+    cloneResponse(input.value, `Ouaiiiiis bravo champion !!!`)
+    
+  }
+  input.value = ''
+}
+
+document.querySelector('button#submit').onclick = () => {
+  submit()
+}
+
+document.body.onkeydown =(event) => {
+  if (event.key === 'Enter'){
+    submit()
+  }
+}
